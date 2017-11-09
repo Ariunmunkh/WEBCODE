7fb3d8b6597b61ffe94dbf7d1937f0705e16f020
Ariunmunkh.e <is11d011@gmail.com>
2017-11-10 00:37:40 +0800

    edited by Muugii

--- WebCode/WebCode/Controllers/HomeController.cs
+++ WebCode/WebCode/Controllers/HomeController.cs
@@ -8,11 +8,56 @@ namespace WebCode.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        List<MenuItem> mainMenu = new List<MenuItem>();
+        List<MenuItem> childMenu = new List<MenuItem>();
+
+        public HomeController()
         {
-            return View();
+            mainMenu = new List<MenuItem>()
+            {
+                new MenuItem() { id = 1, name = "Home", action = "Index", parentId = null},
+                new MenuItem() { id = 2, name = "About", action = "About", parentId = null},
+                new MenuItem() { id = 3, name = "Events", action = "EventDetail", parentId = null},
+                new MenuItem() { id = 4, name = "Gallery", action = "", parentId = null},
+                new MenuItem() { id = 5, name = "Causes", action = "", parentId = null},
+                new MenuItem() { id = 6, name = "Page", action = "", parentId = null},
+                new MenuItem() { id = 7, name = "Blog", action = "", parentId = null},
+                new MenuItem() { id = 8, name = "Contact", action = "Contact", parentId = null}
+            };
+
+            childMenu = new List<MenuItem>()
+            {
+                new MenuItem() { id = 9, name = "About us one", action = "About", parentId = 2},
+                new MenuItem() { id = 10, name = "About us two", action = "", parentId = 2},
+                new MenuItem() { id = 11, name = "Event details", action = "EventDetails", parentId = 3},
+                new MenuItem() { id = 12, name = "Gallery one", action = "", parentId = 4},
+                new MenuItem() { id = 13, name = "Gallery two", action = "", parentId = 4},
+                new MenuItem() { id = 15, name = "Gallery three", action = "", parentId = 4},
+                new MenuItem() { id = 16, name = "Gallery four", action = "", parentId = 4},
+                new MenuItem() { id = 17, name = "Cause details one", action = "", parentId = 5},
+                new MenuItem() { id = 18, name = "Cause details two", action = "", parentId = 5},
+                new MenuItem() { id = 18, name = "Staff page", action = "", parentId = 6},
+                new MenuItem() { id = 18, name = "Staff profile", action = "", parentId = 6},
+                new MenuItem() { id = 18, name = "Staff profile two", action = "", parentId = 6},
+                new MenuItem() { id = 18, name = "Donation", action = "", parentId = 6},
+                new MenuItem() { id = 18, name = "Faq", action = "", parentId = 6},
+                new MenuItem() { id = 18, name = "Error-404", action = "", parentId = 6},
+                new MenuItem() { id = 18, name = "Blog one", action = "BlogV1", parentId = 7},
+                new MenuItem() { id = 18, name = "Blog two", action = "", parentId = 7},
+                new MenuItem() { id = 18, name = "Blog three", action = "", parentId = 7},
+                new MenuItem() { id = 18, name = "Blog details", action = "", parentId = 7}
+            };
         }
 
+        public ActionResult Index()
+        {       
+            HomeItem home = new HomeItem();
+            home.menuitem = mainMenu;
+            home.childitem = childMenu;
+            return View(home);
+        }
+
+        [HttpGet]
         public ActionResult Index2()
         {
             ViewBag.Message = "Your contact page.";
@@ -30,10 +75,13 @@ namespace WebCode.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
-            return View();
+            HomeItem home = new HomeItem();
+            home.menuitem = mainMenu;
+            home.childitem = childMenu;
+            return View(home);
         }
 
+        [HttpGet]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -41,12 +89,47 @@ namespace WebCode.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult EventDetails()
         {
-            ViewBag.Message = "Your application description page.";
+            HomeItem home = new HomeItem();
+            home.menuitem = mainMenu;
+            home.childitem = childMenu;
 
-            return View();
+            return View(home);
         }
 
+        [HttpGet]
+        public ActionResult BlogV1()
+        {
+            HomeItem home = new HomeItem();
+            home.menuitem = mainMenu;
+            home.childitem = childMenu;
+
+            return View(home);
+        }
+
+        [HttpGet]
+        public ActionResult _MenuPartial()
+        {
+            HomeItem home = new HomeItem();
+            home.menuitem = mainMenu;
+            home.childitem = childMenu;
+            return View(home);
+        }
+    }
+
+    public class HomeItem
+    {
+        public List<MenuItem> menuitem { get; set; }
+        public List<MenuItem> childitem { get; set; }
+    }
+
+    public class MenuItem
+    {
+        public int id{ get; set; }
+        public string name { get; set; }
+        public string action { get; set; }
+        public int? parentId { get; set; }
     }
 }
